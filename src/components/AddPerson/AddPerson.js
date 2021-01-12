f88fbae14901256cdd1310ffa103c2b88b6a2f15
binarytrance <ganeshan.dash@gmail.com>
2021-01-13 01:21:47 +0530

    done

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
+const addPerson = (props) => {
+    return (
+        <div className="AddPerson">
+            <button onClick={props.personAdded}>Add Personseeessss, my precious!</button>
+        </div>
+    )
+};
 
 export default addPerson;
--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -2,43 +2,60 @@ import React, { Component } from 'react';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
-
+import { connect } from 'react-redux';
+import * as actionTypes from '../store/actionTypes';
 class Persons extends Component {
-    state = {
-        persons: []
-    }
+    // state = {
+    //     persons: []
+    // }
 
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
+    // personAddedHandler = () => {
+    //     const newPerson = {
+    //         id: Math.random(), // not really unique but good enough here!
+    //         name: 'Max',
+    //         age: Math.floor( Math.random() * 40 )
+    //     }
+    //     this.setState( ( prevState ) => {
+    //         return { persons: prevState.persons.concat(newPerson)}
+    //     } );
+    // }
 
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
+    // personDeletedHandler = (personId) => {
+    //     this.setState( ( prevState ) => {
+    //         return { persons: prevState.persons.filter(person => person.id !== personId)}
+    //     } );
+    // }
+
 
     render () {
+        console.log('props', this.props.personAddedHandler);
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
+                <AddPerson personAdded={this.props.personAddedHandler} />
+                {this.props.persons.map(person => (
+                    <Person
                         key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        name={person.name}
+                        age={person.age}
+                        clicked={() => this.props.personDeleteHandler(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+    // console.log(state)
+    return {
+        persons: state.persons
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        personAddedHandler: () => dispatch({type: actionTypes.ADD_PERSON }),
+        personDeleteHandler: (personId) => dispatch({type: actionTypes.DELETE_PERSON, personId: personId})
+    }
+}
+// console.log('asdf',mapStateToProps, mapDispatchToProps)
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
